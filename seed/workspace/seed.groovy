649e2297c851b6730fdbfc2d89252a12bc717e95
skytap <Victor Cabello>
2015-11-10 14:17:29 -0800

    Firs pipeline draft

--- seed/workspace/seed.groovy
+++ seed/workspace/seed.groovy
@@ -1,1 +1,31 @@
 println "initializing...."
+
+job('Build'){
+  steps {
+    shell( 'echo \'Building code\'')
+  }
+}
+
+job('Deploy'){
+  steps {
+    shell( 'echo \'Deploying war\'')
+  }
+  triggers {
+    upstream('Build')
+  }
+}
+
+job('Testing'){
+  steps {
+    shell( 'echo \'Testing app\'')
+  }
+  triggers {
+    upstream('Deploy')
+  }
+}
+
+
+
+buildPipelineView('Pipeline'){
+  selectedJob('Build')
+}
